3e49c8b689025cda8efea634914970ba6955dd7b
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-01 12:45:45 +0000

    Initial Commit

---
+++ Conentration_Challenge/Conentration_ChallengeApp.swift
@@ -0,0 +1,18 @@
+//
+//  Conentration_ChallengeApp.swift
+//  Conentration_Challenge
+//
+//  Created by app-kaihatsusha on 01/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Conentration_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
